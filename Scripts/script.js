581c8965b7076b2a4ac03d148eb2496cc8dafcc6
Austin-TG <73093374+Austin-TG@users.noreply.github.com>
2022-12-06 10:26:38 -0700

    Updated header/body, script updated

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by javascript";
 
-document.querySelector("body").appendChild(h2);
+document.getElementById("mainHeaderContainer").appendChild(h2);
