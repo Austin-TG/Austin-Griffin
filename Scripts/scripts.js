c274015ebf6f9feb392af10e9f5fe008da015e2b
Austin Griffin <austintg94@gmail.com>
2022-12-25 18:30:10 -0700

    Created new scripts.js and styles.css files

---
+++ Scripts/scripts.js
@@ -0,0 +1,78 @@
+/* Place your JavaScript in this file */
+
+function headerDisplay() {
+    document.getElementById("headerText").innerHTML = "<p> Austin Griffin's Portfolio </p>";
+}
+
+function displayContent() {
+    document.getElementById("content").style.display = "block";
+    document.getElementById("content1").style.display = "none";
+    document.getElementById("content2").style.display = "none";
+    document.getElementById("content3").style.display = "none";
+    document.getElementById("content4").style.display = "none";
+    document.getElementById("content5").style.display = "none";
+    document.getElementById("content6").style.display = "none";
+}
+function displayContent1() {
+    document.getElementById("content").style.display = "none";
+    document.getElementById("content1").style.display = "block";
+    document.getElementById("content2").style.display = "none";
+    document.getElementById("content3").style.display = "none";
+    document.getElementById("content4").style.display = "none";
+    document.getElementById("content5").style.display = "none";
+    document.getElementById("content6").style.display = "none";
+}
+function displayContent2() {
+    document.getElementById("content").style.display = "none";
+    document.getElementById("content1").style.display = "none";
+    document.getElementById("content2").style.display = "block";
+    document.getElementById("content3").style.display = "none";
+    document.getElementById("content4").style.display = "none";
+    document.getElementById("content5").style.display = "none";
+    document.getElementById("content6").style.display = "none";
+}
+function displayContent3() {
+    document.getElementById("content").style.display = "none";
+    document.getElementById("content1").style.display = "none";
+    document.getElementById("content2").style.display = "none";
+    document.getElementById("content3").style.display = "block";
+    document.getElementById("content4").style.display = "none";
+    document.getElementById("content5").style.display = "none";
+    document.getElementById("content6").style.display = "none";
+}
+function displayContent4() {
+    document.getElementById("content").style.display = "none";
+    document.getElementById("content1").style.display = "none";
+    document.getElementById("content2").style.display = "none";
+    document.getElementById("content3").style.display = "none";
+    document.getElementById("content4").style.display = "block";
+    document.getElementById("content5").style.display = "none";
+    document.getElementById("content6").style.display = "none";
+}
+function displayContent5() {
+    document.getElementById("content").style.display = "none";
+    document.getElementById("content1").style.display = "none";
+    document.getElementById("content2").style.display = "none";
+    document.getElementById("content3").style.display = "none";
+    document.getElementById("content4").style.display = "none";
+    document.getElementById("content5").style.display = "block";
+    document.getElementById("content6").style.display = "none";
+}
+function displayContent6() {
+    document.getElementById("content").style.display = "none";
+    document.getElementById("content1").style.display = "none";
+    document.getElementById("content2").style.display = "none";
+    document.getElementById("content3").style.display = "none";
+    document.getElementById("content4").style.display = "none";
+    document.getElementById("content5").style.display = "none";
+    document.getElementById("content6").style.display = "block";
+}
+
+function footerYearDisplay() {
+    const today = new Date();
+    const year = today.getFullYear(); 
+    return year;
+}
+function webpageLoad() {
+    headerDisplay();
+}
